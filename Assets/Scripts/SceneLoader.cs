e4a6f19d2b9020117dccb1a145558a1bfb9984e3
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-19 17:34:07 -0600

    finishing prototype touches

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+}
