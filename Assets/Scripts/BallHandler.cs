99dcc8cc22d300268a1b1c2c38dc172d65e7efaf
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-19 15:46:59 -0600

    Ball spawns and respawns

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,15 +6,20 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D currentBallRigidbody;
-    [SerializeField] private SpringJoint2D currentBallSpringJoint;
-    [SerializeField] private float detachDelay = .5f;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+    private Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSpringJoint;
     private Camera mainCamera;
     private bool isDragging;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         mainCamera = Camera.main;
+        SpawnNewBall();
     }
 
     // Update is called once per frame
@@ -47,6 +52,17 @@ public class BallHandler : MonoBehaviour
 
     }
 
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
     private void LaunchBall()
     {
         currentBallRigidbody.isKinematic = false;
@@ -59,5 +75,7 @@ public class BallHandler : MonoBehaviour
     {
         currentBallSpringJoint.enabled = false;
         currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 }
